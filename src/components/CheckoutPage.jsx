126774f80b5bd0525a18d78c04b7524157224b46
Parvesh Qaiser <ahmed75parvesh@gmail.com>
2025-07-03 19:51:14 +0530

    added razopay

--- src/components/CheckoutPage.jsx
+++ src/components/CheckoutPage.jsx
@@ -48,17 +48,36 @@ const CheckoutPage = () => {
             products : product
         };
 
+        let createOrder = { 
+            name : user?.name,
+            email : user?.email,
+            amount :  cart.reduce((sum, book)=> sum + book?.newPrice,0),
+        };
+
+
         try {
-            let res = await axios.post(BASE_URL + "/order/book", data, {withCredentials:true});
-            if(res.data.success)
-            {
-                toast.success(res.data.message, {duration:2000});
-                dispatch(addOrderDetails(res.data.data));
-                dispatch(clearCart());
-                setTimeout(()=>{
-                    navigate("/order/details")                    
-                },2000)                
-            }
+            let res = await axios.post(BASE_URL + "/create/paymnent" ,createOrder, {withCredentials: true});
+            let {userId , orderId , keyId , amount, currency} = res?.data?.data
+
+                const options = {
+                    key: keyId,
+                    amount: amount, 
+                    currency: 'USD',
+                    name: 'DevTinder',
+                    description: 'Welcome to Razorpay',
+                    order_id: orderId, 
+                    prefill: {
+                        name: user?.name,
+                        email: user?.email,
+                        contact: user?.number
+                    },
+                    theme: {
+                        color: '#F37254'
+                    },
+                };
+                const rzp = new window.Razorpay(options);
+                rzp.open();
+
         } catch (error) {
             toast.error(error?.response?.data?.message || error?.message, {duration:2000})
         }
@@ -66,50 +85,50 @@ const CheckoutPage = () => {
 
     return (
     <>
-        <main className="max-w-5xl mx-auto px-4 py-8 my-20 space-y-6 bg-white rounded-lg shadow-sm">
-            <section className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-2">
-                <p className="text-lg text-gray-700 font-medium">🧾 Total Items: <span className="font-semibold">{cart.reduce((sum,book)=> sum + book?.qty,0)}</span></p>
-                <p className="text-lg text-gray-700 font-medium">💰 Amount Payable: <span className="font-semibold text-green-600">$ {cart.reduce((sum, book)=> sum + book?.newPrice,0)}</span></p>
-            </section>
+    <main className="max-w-5xl mx-auto px-4 py-8 my-20 space-y-6 bg-white rounded-lg shadow-sm">
+        <section className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-2">
+            <p className="text-lg text-gray-700 font-medium">🧾 Total Items: <span className="font-semibold">{cart.reduce((sum,book)=> sum + book?.qty,0)}</span></p>
+            <p className="text-lg text-gray-700 font-medium">💰 Amount Payable: <span className="font-semibold text-green-600">$ {cart.reduce((sum, book)=> sum + book?.newPrice,0)}</span></p>
+        </section>
 
-            <h1 className="text-lg font-semibold text-gray-700">📦 Please Select the Billing Address</h1>
-            
-            {(user && user?.address?.length > 0) ? user.address.map((val,index)=>(
-                <section className='flex gap-x-6 border border-dashed rounded-md p-2' key={index}>
-                    <div className='flex items-center'>
-                        <input 
-                            type="radio" 
-                            className="h-4 w-4  focus:ring-blue-500 hover:ring-2 hover:ring-blue-400"
-                            checked={selectedIndex == index} 
-                            onChange={()=>handleChange(index)} 
-                        />
-                    </div>
-                    <div className='space-y-1'>
-                        <h1 className='font-semibold'>{user?.name}</h1>
-                        <h1 className=''>{val?.doorNo} </h1>
-                        <p className='text-sm'>{val?.city} , {val?.state} , {val?.pinCode}</p>
-                        <p className='text-sm'>Phone number : {user?.number}</p>
-                    </div>
-                </section>
-            )): <p className='text-center font-extrabold text-lg text-gray-500'>No Address Found. Please Go to Profile to add Address</p>}
-
-        {selectedIndex !==null && 
-        <>
-            <div className="flex items-center gap-2">
-                <input type="checkbox" onChange={(e)=> setIsCheck(e.target.checked)} className="accent-purple-500 w-4 h-4" />
-                <span className="text-sm text-gray-600">I agree to the <span className="underline cursor-pointer">terms & conditions</span></span>
-            </div>
+        <h1 className="text-lg font-semibold text-gray-700">📦 Please Select the Billing Address</h1>
         
-            <div className="flex justify-end">
-                <button
-                    onClick={handlePlaceOrder}
-                    disabled={!isCheck}
-                    className={`px-4 py-2 border border-purple-600 text-violet-500 rounded-md transition text-sm font-medium ${isCheck? "cursor-pointer" : "cursor-not-allowed"}`}>
-                🛍️ Place Order
-                </button>
-            </div>      
-        </>}                 
-        </main>
+        {(user && user?.address?.length > 0) ? user.address.map((val,index)=>(
+            <section className='flex gap-x-6 border border-dashed rounded-md p-2' key={index}>
+                <div className='flex items-center'>
+                    <input 
+                        type="radio" 
+                        className="h-4 w-4  focus:ring-blue-500 hover:ring-2 hover:ring-blue-400"
+                        checked={selectedIndex == index} 
+                        onChange={()=>handleChange(index)} 
+                    />
+                </div>
+                <div className='space-y-1'>
+                    <h1 className='font-semibold'>{user?.name}</h1>
+                    <h1 className=''>{val?.doorNo} </h1>
+                    <p className='text-sm'>{val?.city} , {val?.state} , {val?.pinCode}</p>
+                    <p className='text-sm'>Phone number : {user?.number}</p>
+                </div>
+            </section>
+        )): <p className='text-center font-extrabold text-lg text-gray-500'>No Address Found. Please Go to Profile to add Address</p>}
+
+    {selectedIndex !==null && 
+    <>
+        <div className="flex items-center gap-2">
+            <input type="checkbox" onChange={(e)=> setIsCheck(e.target.checked)} className="accent-purple-500 w-4 h-4" />
+            <span className="text-sm text-gray-600">I agree to the <span className="underline cursor-pointer">terms & conditions</span></span>
+        </div>
+    
+        <div className="flex justify-end">
+            <button
+                onClick={handlePlaceOrder}
+                disabled={!isCheck}
+                className={`px-4 py-2 border border-purple-600 text-violet-500 rounded-md transition text-sm font-medium ${isCheck? "cursor-pointer" : "cursor-not-allowed"}`}>
+            🛍️ Place Order
+            </button>
+        </div>      
+    </>}                 
+    </main>
     </>
     )
 }
