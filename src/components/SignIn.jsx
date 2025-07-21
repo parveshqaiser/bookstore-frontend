a3c0c6fa22fb22bcd1966541dd485258c436e158
Parvesh Qaiser <ahmed75parvesh@gmail.com>
2025-07-21 22:38:10 +0530

    sign in page modified

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -119,150 +119,150 @@ const SignIn = () => {
             }
         }  
     }
-
+// className='max-w-6xl md:mx-auto my-10 mx-4 shadow-sm pb-4 relative bg-white'
     return (
-    <main className='max-w-6xl md:mx-auto my-10 mx-4 shadow-sm pb-4 relative bg-white'>
+    <main className='flex justify-center items-center min-h-screen bg-gradient-to-br from-amber-50 to-orange-100'>
 
-        <img src={loginLogo} alt='Book logo' className='w-40  mx-auto rounded-full'/>
+        <div className='shadow-sm bg-white p-3 w-full max-w-6xl relative'>
+            <img src={loginLogo} alt='Book logo' className='w-40  mx-auto rounded-full'/>
 
-        <h1 className="md:text-2xl text-lg font-bold text-purple-700 mb-2 text-center">The Book Story Shop</h1>
-        <p className="text-gray-600 text-center">{newUser ? "Create your account" : "Welcome back"}</p>
-        <div className="w-12 h-1 bg-gradient-to-r from-purple-600 to-blue-600 rounded-full mx-auto"></div>
-
-        <section className='w-full max-w-sm mx-auto my-2'> 
+            <h1 className="md:text-2xl text-lg font-bold text-purple-700 mb-2 text-center">The Book Story Shop</h1>
+            <p className="text-gray-600 text-center">{newUser ? "Create your account" : "Welcome back"}</p>
+            <div className="w-12 h-1 bg-gradient-to-r from-purple-600 to-blue-600 rounded-full mx-auto"></div>
+            <section className='w-full max-w-sm mx-auto my-2'> 
             {newUser && 
-            (<div className='mb-4'>
-                <input 
-                    type="text" 
-                    value={inputValues.name}
-                    onChange={(e)=> {
-                        let val = e.target.value;
-                        if(val){
-                            let up = val.charAt(0).toUpperCase() + val.slice(1);
-                            setInputValues({...inputValues , name : up})
-                        }else{
-                            setInputValues({...inputValues , name : ""})
-                        }
-                    }}
-                    placeholder='Enter Full Name' 
-                    // className="py-2 px-2 w-full rounded-md focus:outline-none focus:ring-2 focus:ring-purple-400 bg-blue-50/60 text-gray-600" 
-                    className="w-full px-3 py-3 border border-gray-200 rounded-xl focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent transition-all duration-200 bg-gray-50 hover:bg-white"
-                />
-            </div>)}
-            <div className='mb-4'>
-                <input 
-                    type="text"
-                    value={inputValues.email}
-                    onChange={(e)=>setInputValues({...inputValues, email : e.target.value.trim() || ''})}
-                    placeholder='Enter Email' 
-                    className="w-full px-3 py-3 border border-gray-200 rounded-xl focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent transition-all duration-200 bg-gray-50 hover:bg-white"
-                />
-            </div>
-            <div className='mb-4 flex items-center relative'>
-                <input 
-                    type={toggleIcons ? "text" : "password"} 
-                    placeholder='Enter Password'
-                    onKeyUp={handlePasswordValidation}
-                    onFocus={()=> setIsPasswordFocus(true)}
-                    onChange={(e) =>{
-                        let {value} = e.target;
-                        value = value.trim() || "";
-                        setInputValues({ ...inputValues, password: value}),
-                        setValidations({
-                            length: value.length >= 8,
-                            symbol: /[\W_]/.test(value),
-                            number: /\d/.test(value),
-                            lowerCase: /[a-z]/.test(value),
-                            upperCase: /[A-Z]/.test(value),
-                        });
-                    }}
-                    value={inputValues.password}
-                     className="w-full px-3 py-3 border border-gray-200 rounded-xl focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent transition-all duration-200 bg-gray-50 hover:bg-white"
-                />
-                <button
-                    type="button"
-                    className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer"
-                    onClick={() => setToggleIcons(!toggleIcons)}
-                >
-                    {toggleIcons ? <FaEye size={18} /> : <FaEyeSlash size={18} />}
-                </button>
-            </div>
-
-            <div className='mb-2'>
-            {
-                isDisabled ?  
-                    (<div className="text-center">
-                        <ProgressSpinner 
-                            style={{width: '50px', height: '50px'}} 
-                            strokeWidth="8" fill="var(--surface-ground)" 
-                            animationDuration=".5s" 
-                        /> 
-                    <p className="text-gray-600 text-sm">Please Wait...</p>
-                </div> ): (
-                   <button
-                        onClick={handleClick}
-                        disabled={newUser && pwdDisabled}
-                        className={`w-full py-3 px-3 cursor-pointer rounded-xl font-medium transition-all duration-200 ${
-                        newUser && pwdDisabled
-                            ? "bg-gray-100 text-gray-400 cursor-not-allowed"
-                            : "bg-gradient-to-r from-purple-600 to-blue-600 text-white hover:from-purple-700 hover:to-blue-700 transform hover:scale-[1.02] shadow-lg hover:shadow-xl"
-                        }`}
-                    >
-                        {newUser ? "Create Account" : "Sign In"}
-                    </button>
-                    )
-                }
-            </div>
-
-            <div className="mt-6 text-center">
-                <p className="text-sm text-gray-600">
-                    {!newUser ? " Don't have an account? " : " Already have an account? "}
-                    <button
-                        onClick={() => {
-                            setNewUser(!newUser)
-                            setInputValues({ name: "", email: "", password: "" })
-                            setIsPasswordFocus(false)
+                (<div className='mb-4'>
+                    <input 
+                        type="text" 
+                        value={inputValues.name}
+                        onChange={(e)=> {
+                            let val = e.target.value;
+                            if(val){
+                                let up = val.charAt(0).toUpperCase() + val.slice(1);
+                                setInputValues({...inputValues , name : up})
+                            }else{
+                                setInputValues({...inputValues , name : ""})
+                            }
                         }}
-                        className="text-purple-600 hover:text-purple-700 font-medium hover:underline transition-colors cursor-pointer "
+                        placeholder='Enter Full Name' 
+                        // className="py-2 px-2 w-full rounded-md focus:outline-none focus:ring-2 focus:ring-purple-400 bg-blue-50/60 text-gray-600" 
+                        className="w-full px-3 py-3 border border-gray-200 rounded-xl focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent transition-all duration-200 bg-gray-50 hover:bg-white"
+                    />
+                </div>)}
+                <div className='mb-4'>
+                    <input 
+                        type="text"
+                        value={inputValues.email}
+                        onChange={(e)=>setInputValues({...inputValues, email : e.target.value.trim() || ''})}
+                        placeholder='Enter Email' 
+                        className="w-full px-3 py-3 border border-gray-200 rounded-xl focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent transition-all duration-200 bg-gray-50 hover:bg-white"
+                    />
+                </div>
+                <div className='mb-4 flex items-center relative'>
+                    <input 
+                        type={toggleIcons ? "text" : "password"} 
+                        placeholder='Enter Password'
+                        onKeyUp={handlePasswordValidation}
+                        onFocus={()=> setIsPasswordFocus(true)}
+                        onChange={(e) =>{
+                            let {value} = e.target;
+                            value = value.trim() || "";
+                            setInputValues({ ...inputValues, password: value}),
+                            setValidations({
+                                length: value.length >= 8,
+                                symbol: /[\W_]/.test(value),
+                                number: /\d/.test(value),
+                                lowerCase: /[a-z]/.test(value),
+                                upperCase: /[A-Z]/.test(value),
+                            });
+                        }}
+                        value={inputValues.password}
+                        className="w-full px-3 py-3 border border-gray-200 rounded-xl focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent transition-all duration-200 bg-gray-50 hover:bg-white"
+                    />
+                    <button
+                        type="button"
+                        className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer"
+                        onClick={() => setToggleIcons(!toggleIcons)}
                     >
-                        {!newUser ? "Sign up here" : "Sign in"}
+                        {toggleIcons ? <FaEye size={18} /> : <FaEyeSlash size={18} />}
                     </button>
+                </div>
+
+                <div className='mb-2'>
+                {
+                    isDisabled ?  
+                        (<div className="text-center">
+                            <ProgressSpinner 
+                                style={{width: '50px', height: '50px'}} 
+                                strokeWidth="8" fill="var(--surface-ground)" 
+                                animationDuration=".5s" 
+                            /> 
+                        <p className="text-gray-600 text-sm">Please Wait...</p>
+                    </div> ): (
+                    <button
+                            onClick={handleClick}
+                            disabled={newUser && pwdDisabled}
+                            className={`w-full py-3 px-3 cursor-pointer rounded-xl font-medium transition-all duration-200 ${
+                            newUser && pwdDisabled
+                                ? "bg-gray-100 text-gray-400 cursor-not-allowed"
+                                : "bg-gradient-to-r from-purple-600 to-blue-600 text-white hover:from-purple-700 hover:to-blue-700 transform hover:scale-[1.02] shadow-lg hover:shadow-xl"
+                            }`}
+                        >
+                            {newUser ? "Create Account" : "Sign In"}
+                        </button>
+                        )
+                    }
+                </div>
+
+                <div className="mt-6 text-center">
+                    <p className="text-sm text-gray-600">
+                        {!newUser ? " Don't have an account? " : " Already have an account? "}
+                        <button
+                            onClick={() => {
+                                setNewUser(!newUser)
+                                setInputValues({ name: "", email: "", password: "" })
+                                setIsPasswordFocus(false)
+                            }}
+                            className="text-purple-600 hover:text-purple-700 font-medium hover:underline transition-colors cursor-pointer "
+                        >
+                            {!newUser ? "Sign up here" : "Sign in"}
+                        </button>
+                    </p>
+                </div>
+
+                <p className='text-sm text-center'>
+                    <Link to="/" className="inline-flex items-center text-sm text-gray-500 hover:text-gray-700 transition-colors" >
+                        Go to Home <FaArrowRightLong className='inline ml-1' />
+                    </Link>
                 </p>
-            </div>
+                {!newUser && (
+                <p className='text-sm text-center text-blue-400'>
+                    <Link to="/forgot/password" className='hover:underline'>Forgot Password ?</Link>
+                </p>)}           
+            </section>
 
-            <p className='text-sm text-center'>
-                <Link to="/" className="inline-flex items-center text-sm text-gray-500 hover:text-gray-700 transition-colors" >
-                    Go to Home <FaArrowRightLong className='inline ml-1' />
-                </Link>
-            </p>
-            {!newUser && (
-            <p className='text-sm text-center text-blue-400'>
-                <Link to="/forgot/password" className='hover:underline'>Forgot Password ?</Link>
-            </p>)}
-           
-        </section>
-
-        {(newUser && isPaswordFocus) &&  
-        <aside className={`passwordValidationList absolute rounded-lg shadow-lg border border-gray-100 p-4 z-10`}>
-            <h4 className="text-sm font-medium text-gray-700 mb-2">Password Requirements:</h4>
-            <div className='text-sm font-medium font-serif'>
-                <div className={validations.length ? 'valid' : 'error'}>
-                At least <b>8 characters</b>
+            {(newUser && isPaswordFocus) &&  
+            <aside className={`passwordValidationList absolute rounded-lg shadow-lg border border-gray-100 p-4 z-10`}>
+                <h4 className="text-sm font-medium text-gray-700 mb-2">Password Requirements:</h4>
+                <div className='text-sm font-medium font-serif'>
+                    <div className={validations.length ? 'valid' : 'error'}>
+                    At least <b>8 characters</b>
+                    </div>
+                    <div className={validations.symbol ? 'valid' : 'error'}>
+                        At least <b>one symbol</b>
+                    </div>
+                    <div className={validations.number ? 'valid' : 'error'}>
+                        At least <b>one number</b>
+                    </div>
+                    <div className={validations.lowerCase ? 'valid' : 'error'}>
+                        At least <b>one lower case</b>
+                    </div>
+                    <div className={validations.upperCase ? 'valid' : 'error'}>
+                        At least <b>one upper case</b>
+                    </div>
                 </div>
-                <div className={validations.symbol ? 'valid' : 'error'}>
-                    At least <b>one symbol</b>
-                </div>
-                <div className={validations.number ? 'valid' : 'error'}>
-                    At least <b>one number</b>
-                </div>
-                <div className={validations.lowerCase ? 'valid' : 'error'}>
-                    At least <b>one lower case</b>
-                </div>
-                <div className={validations.upperCase ? 'valid' : 'error'}>
-                    At least <b>one upper case</b>
-                </div>
-            </div>
-        </aside>}
+            </aside>}
+        </div>     
     </main>
     )
 }
